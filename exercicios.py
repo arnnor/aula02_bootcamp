6d010cdb0f1840b5f408c05b08203529718802cc
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-24 11:55:18 -0300

    iniciando o exercio #14

--- exercicios.py
+++ exercicios.py
@@ -31,6 +31,9 @@ print(f"Essa é a área do circulo de raio = {raio_do_circulo}: {area_do_circulo
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+input
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
