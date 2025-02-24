6cd1c15d2ff41e86164b20b3b397d3412fd40fb5
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-24 11:53:20 -0300

    exercicio #10

--- exercicios.py
+++ exercicios.py
@@ -18,7 +18,12 @@ print(f"A soma dos numeros é: {resultado}")
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
+raio_do_circulo = float(input("Digite o raio do circulo: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"Essa é a área do circulo de raio = {raio_do_circulo}: {area_do_circulo: .2f}")
 
 # #### Strings (`str`)
 
