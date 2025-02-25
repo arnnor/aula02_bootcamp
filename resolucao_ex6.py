ad066cd3614f7576541a5f2ed28453219f67d0aa
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:56:59 -0300

    Resolucação do exercício 6.

---
+++ resolucao_ex6.py
@@ -0,0 +1,7 @@
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+num1 = float(input("Digite o primeiro flutuante: "))
+num2 = float(input("Digite o segundo número flutuante: "))
+
+resultado_soma = num1 + num2
+
+print("A soma é:", resultado_soma)
