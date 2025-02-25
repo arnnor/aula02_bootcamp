053c08d8a1783d6d5c1ecd4762039d2e8add3a66
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:42:45 -0300

    Resolucação do exercicio 5.

---
+++ resolucao_ex5.py
@@ -0,0 +1,4 @@
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+num = int(input("Digite um número para ser calculado o quadrado: "))
+resultado_quadrado = num ** 2
+print("O quadrado do número é:", resultado_quadrado)
