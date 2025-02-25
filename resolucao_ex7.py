5b02aec3fa3166691489a413c1e4d7f157db08fc
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 17:10:11 -0300

    Resolucação exercício 7.

---
+++ resolucao_ex7.py
@@ -0,0 +1,7 @@
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+num1 = float(input("Digite o primeiro número flutuante: "))
+num2 = float(input("Digite o segundo número flutuante: "))
+
+media = (num1 + num2)/2
+
+print("A média é:", media)
