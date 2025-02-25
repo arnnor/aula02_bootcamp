63393db69e18b45f0ed8a9f01fbc32540a11679c
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:35:32 -0300

    Resolucao do exercicios 3.

---
+++ resolucao_ex3.py
@@ -0,0 +1,5 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+resultado_multiplicacao = num1 * num2
+print("O resultado da multiplicacao é:", resultado_multiplicacao)
