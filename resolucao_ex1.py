36a4a13182777014bc89cdb42c8d71ae3dc35ac1
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:44:52 -0300

    Resolução do exercicio 1.

--- resolucao_ex1.py
+++ resolucao_ex1.py
@@ -1,6 +1,6 @@
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Insirir outro numero inteiro: "))
+numero_01 = int(input("Inserir o primeiro numero inteiro: "))
+numero_02 = int(input("Insirir o segundo numero inteiro: "))
 resultado = numero_01 + numero_02
 print(f"A soma dos numeros é: {resultado}")
