ead4d573629549bed81da425a2f9b935442cea94
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:20:17 -0300

    No arquivo exercicios.py só teremos os exercicios propostos.

---
+++ resolucao_ex1.py
@@ -0,0 +1,6 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Insirir outro numero inteiro: "))
+resultado = numero_01 + numero_02
+print(f"A soma dos numeros é: {resultado}")
