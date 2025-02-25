34bf9c89331a7bc75bc6e3ecd24a8635bb317435
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 16:45:48 -0300

    Resolução do exercicio 2.

--- resolucao_ex2.py
+++ resolucao_ex2.py
@@ -1,4 +1,4 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-num = int(input("Digite um número: "))
+num = int(input("Digite um número inteiro: "))
 resultado_resto = num % 5
 print("O resto da divisão por 5 é: ", resultado_resto)
