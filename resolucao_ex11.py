f72341af7fce9be0ffdd7a8ba054faa93ed7dec6
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 17:48:01 -0300

    Resolução do exercício 11.

---
+++ resolucao_ex11.py
@@ -0,0 +1,7 @@
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+texto = str(input("Digite um texto:"))
+
+texto_maiusculas = texto.upper()
+
+print("Texto em maiúsculas:", texto_maiusculas)
