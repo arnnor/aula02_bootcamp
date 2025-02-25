5633f9bf891f3b19b7086af82d01a731c76675eb
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 17:18:31 -0300

    Resolução do exercício 8.

---
+++ resolucao_ex8.py
@@ -0,0 +1,9 @@
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+base = float(input("Digite a base da potência: "))
+
+expoente = float(input("Digite o expoente da potência: "))
+
+potencia = base ** expoente
+
+print("O resultado da potência é: ", potencia)
