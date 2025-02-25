7b85c1a5ff5372077375affdf1c3a94b36ba096c
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-25 17:32:12 -0300

    Resolução do exercício 9.

---
+++ resolucao_ex9.py
@@ -0,0 +1,7 @@
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+celcius = float(input("Digite a temperatura em Celcius: "))
+
+fahrenheit = (celcius * 5/9) + 32
+
+print(f"{celcius}C° é igual a {fahrenheit}°F")
