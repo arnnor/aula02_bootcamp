3ca900658fa9f97b6bfe9fa046944d94ceb9617a
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-26 18:46:42 -0300

    Resolução do exercício 16.

---
+++ resolucao_ex16.py
@@ -0,0 +1,7 @@
+# 16. Escreva um programa que avalie duas expressoes booleanas inseridas pelo 
+# usuário e retorne o resultado da operação AND entre elas.
+
+valor1 = True #bool(input("Insira o valor boleano da saída 1: "))
+valor2 = False #bool(input("Insira  valor boleano da saída 2: "))
+resultado_and = valor1 and valor2
+print("Resultado do AND lógico:", resultado_and)
