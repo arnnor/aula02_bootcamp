44858994880260c570c13f7d0465f416f9f23330
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-26 18:23:57 -0300

    Resolução do exercício 15.

---
+++ resolucao_ex15.py
@@ -0,0 +1,12 @@
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+nome = str(input("Digite o seu primeiro nome: "))
+
+sobrenome = str(input("Digite o seu sobrenome: "))
+
+espaco = " "
+
+nome_completo = nome + espaco + sobrenome
+
+print("Nome completo:", nome_completo)
+
