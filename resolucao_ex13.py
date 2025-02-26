c7a5b0bbfbd94681789c8e749e5a36f765509a15
arnnor rocha <arnnor.rocha@gmail.com>
2025-02-26 18:08:12 -0300

    Resolução do exercício 13.

---
+++ resolucao_ex13.py
@@ -0,0 +1,9 @@
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, 
+# imprima esta frase sem espaços em branco no início e no final.
+
+frase = str(input("Digite uma frase: "))
+
+frase_sem_espacos = frase.strip()
+
+print("Frase sem espações no início e no final:", frase_sem_espacos)
+
