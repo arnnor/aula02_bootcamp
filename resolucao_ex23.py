70c3340e1aaa885f4329cf982e5504a7f7a3e40a
arnnor rocha <arnnor.rocha@gmail.com>
2025-03-01 09:48:03 -0300

    Resolução do exercício 23.

--- resolucao_ex23.py
+++ resolucao_ex23.py
@@ -1,3 +1,10 @@
+# Exercício 23: Calculadora Simples
+# Desenvolva uma calculadora simples que aceite duas entradas numéricas 
+# e um operador (+, -, *, /) do usuário. 
+# Use try-except para lidar com divisões por zero e entradas não numéricas. 
+# Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. 
+# Imprima o resultado ou uma mensagem de erro apropriada.
+
 try:
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo numero: "))
