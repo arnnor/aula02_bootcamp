c1c79afcf79ae7596e2320ecc5ac957908e1c4a4
arnnor rocha <arnnor.rocha@gmail.com>
2025-03-01 09:47:30 -0300

    Resolução do exercício 22.

--- resolucao_ex22.py
+++ resolucao_ex22.py
@@ -1,4 +1,7 @@
-# # 22: Verificador de Palíndromo
+# Exercício 22: Verificador de Palíndromo
+# Crie um programa que verifica se uma palavra ou frase é um palíndromo 
+# (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). 
+# Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
 
 entrada = input("Digite uma palavra ou frase: ")
 
